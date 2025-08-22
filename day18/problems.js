262bbe6e9f0097058dbcfbb585da51c5838efc94
dineshkumar24v <dineshkumar24v@gmail.com>
2025-08-22 23:01:01 +0530

    day18

---
+++ day18/problems.js
@@ -0,0 +1,14 @@
+// Basic level
+// Task 1: take prompt and Convert to Uppercase
+// let str=prompt('enter any string')
+// console.log(str.toUpperCase())
+
+// Task 2: take prompt and Convert to Lowercase
+// let str1=prompt('enter any string')
+// console.log(str1.toLowerCase())
+
+// Task 3: take prompt and Reverse a String  hints: use split and reverse and join method
+let str2=prompt('enter any string')
+
+
+// Task 4: take prompt and Count the number of vowels in a string--hints use match method
